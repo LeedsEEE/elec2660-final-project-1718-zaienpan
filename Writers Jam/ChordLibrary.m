1127b1d78227b2aacf19257455a19d3e72ecfdfe
ZaiEn <Meowmers@zaiens-mac-pro>
2017-11-17 20:33:52 +0000

    all TextFields stores a different PickerView input now

--- Writers Jam/ChordLibrary.m
+++ Writers Jam/ChordLibrary.m
@@ -10,5 +10,7 @@
 
 @implementation ChordLibrary
 
+
+
 @end
 
--- Writers Jam/ViewController.m
+++ Writers Jam/ViewController.m
@@ -22,14 +22,26 @@
 
     //using UIPickerView as input for the chords
     UIPickerView *chordPicker = [[UIPickerView alloc] init];
-    
-    chordPicker.dataSource = self;
     chordPicker.delegate = self;
+    chordPicker.tag = 1;
+    
+    UIPickerView *chordPicker2 = [[UIPickerView alloc] init];
+    chordPicker2.delegate = self;
+    chordPicker2.tag = 2;
+    
+    UIPickerView *chordPicker3 = [[UIPickerView alloc] init];
+    chordPicker3.delegate = self;
+    chordPicker3.tag = 3;
+    
+    UIPickerView *chordPicker4 = [[UIPickerView alloc] init];
+    chordPicker4.delegate = self;
+    chordPicker4.tag = 4;
+    
     
     self.selectChord1.inputView = chordPicker;
-    self.selectChord2.inputView = chordPicker;
-    self.selectChord3.inputView = chordPicker;
-    self.selectChord4.inputView = chordPicker;
+    self.selectChord2.inputView = chordPicker2;
+    self.selectChord3.inputView = chordPicker3;
+    self.selectChord4.inputView = chordPicker4;
 
 }
 
@@ -37,6 +49,19 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    
+    return 1;
+    
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView
+numberOfRowsInComponent:(NSInteger)component{
+    
+    return [self.Cmajor count];
+    
 }
 
 - (NSString *)pickerView:(UIPickerView *)pickerView
@@ -51,25 +76,23 @@
       didSelectRow:(NSInteger)row
        inComponent:(NSInteger)component{
     
-    NSLog(@"Selected Row %d", row);
-
-    {
-            self.selectChord1.text = _Cmajor[row];
+    NSLog(@"Selected Row %ld", (long)row);
+    if (pickerView.tag == 1){
+        self.selectChord1.text = _Cmajor[row];
+    } else if (pickerView.tag == 2){
+        self.selectChord2.text = _Cmajor[row];
+    } else if (pickerView.tag == 3){
+        self.selectChord3.text = _Cmajor[row];
+    } else if (pickerView.tag == 4){
+        self.selectChord4.text = _Cmajor[row];
     }
     
-}
-
-- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
-
-    return 1;
-
-}
-
-- (NSInteger)pickerView:(UIPickerView *)pickerView
-numberOfRowsInComponent:(NSInteger)component{
     
-        return [self.Cmajor count];
-
 }
+
+//touch anywhere to dismiss keyboard/pickerview from stackoverflow
+- (void)touchesBegan:(NSSet<UITouch *> *)touches
+           withEvent:(UIEvent *)event {
+    [self.view endEditing:YES];}
 
 @end
